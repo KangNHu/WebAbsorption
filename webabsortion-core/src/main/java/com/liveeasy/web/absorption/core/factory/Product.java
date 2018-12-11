9ce41c99ed57139a137f4662aedc6057ac1486f5
hukangning <2035711178@qq.com>
2018-12-11 14:16:28 +0800

    完成贝壳的规则匹配

--- webabsortion-core/src/main/java/com/liveeasy/web/absorption/core/factory/Product.java
+++ webabsortion-core/src/main/java/com/liveeasy/web/absorption/core/factory/Product.java
@@ -1,1 +1,1 @@
-package com.liveeasy.web.absorption.core.factory;public interface Product extends Cloneable{    Product createClone() throws CloneNotSupportedException;}
+package com.liveeasy.web.absorption.core.factory;/** * 克隆接口 * {@link WebAbsorptionComponentCloneBeanFactory}克隆的对象必须实现该接口 */public interface Product extends Cloneable{    /**     * 创建克隆对象     * @return     * @throws CloneNotSupportedException     */    Product createClone() throws CloneNotSupportedException;}
