66115424f5f22329d23cb1439d5a63a7b5e16f6d
hukangning <2035711178@qq.com>
2018-12-21 16:12:41 +0800

    新增了58同城楼盘demo

--- webabsortion-demo-beike/src/main/java/com/liveeeasy/web/absorption/demo/DemoApplication.java
+++ webabsortion-demo-beike/src/main/java/com/liveeeasy/web/absorption/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.liveeeasy.web.absorption.demo;
 
+import com.liveeeasy.web.absorption.demo.spider.wuba.WuBaSpider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,12 +17,20 @@ public class DemoApplication {
 
 
 
+
+
     /**
      *
      * @param args
      */
-    public static void main(String[] args) throws CloneNotSupportedException {
-        SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) throws CloneNotSupportedException, IllegalAccessException {
+        ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
+        runWuBaSpider(run);
     }
 
+
+    public static void runWuBaSpider(ApplicationContext applicationContext) throws IllegalAccessException {
+        WuBaSpider wuBaSpider = applicationContext.getBean(WuBaSpider.class);
+        wuBaSpider.startWuBa();
+    }
 }
